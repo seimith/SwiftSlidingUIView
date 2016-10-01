ea9c74d5ab225daf59e304d1250688fac67564e1
= <seimith@gmail.com>
2016-10-01 12:02:49 -0700

    Sliding UIView

--- SlidingUIView/ViewController.swift
+++ SlidingUIView/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiViewToSlide: UIView!
+    
+    
+    @IBAction func slideUIView () {
+        self.beginAnimation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.beginAnimation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func beginAnimation() {
+        self.uiViewToSlide.transform = CGAffineTransformMakeTranslation(0, -100)
+        
+        UIView.animateWithDuration(0.4, delay: 0, options: [.CurveEaseInOut], animations: {
+            self.uiViewToSlide.transform = CGAffineTransformMakeTranslation(0, 50)
+            }, completion: { completion in
+                UIView.animateWithDuration(0.2, delay: 0.0, options: [.CurveEaseInOut], animations: {
+                    self.uiViewToSlide.transform = CGAffineTransformMakeTranslation(0, 0)
+                }, completion: nil)
+        })
+        
+    }
 }
 
